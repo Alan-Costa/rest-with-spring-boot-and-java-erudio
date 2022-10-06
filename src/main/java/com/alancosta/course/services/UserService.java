1e314c61621bcb20f84ac8f2168baf46ed370ca4
Alan Costa <61335987+Alan-Costa@users.noreply.github.com>
2022-10-06 16:31:46 -0300

    Exception handling - update

--- src/main/java/com/alancosta/course/services/UserService.java
+++ src/main/java/com/alancosta/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.alancosta.course.entities.User;
 import com.alancosta.course.repositories.UserRepository;
 import com.alancosta.course.services.exceptions.DataBaseException;
 import com.alancosta.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +48,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+       try {
+           User entity = repository.getReferenceById(id);
+           updateData(entity, obj);
+           return repository.save(entity);
+       }
+       catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
